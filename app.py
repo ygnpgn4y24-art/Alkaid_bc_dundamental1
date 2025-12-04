6c7b5182033f6007b5a71890ea9d97c1d7fcc2f5
ygnpgn4y24-art <ygnpgn4y24@privaterelay.appleid.com>
2025-12-04 11:01:35 +0000

    同步代码到GitHub

--- app.py
+++ app.py
@@ -6,11 +6,15 @@ app=Flask(__name__)
 def index():
     return(render_template("index.html"))
 
-@app. route("/main", methods=["GET", "POST" ])
+@app.route("/main", methods=["GET", "POST"])
 def main():
- q = request. form. get ("q")
- print (q)
- return (render_template("main.html"))
+    q = request.form.get("q")
+    print(q)
+    return render_template("main.html")
+
+@app.route("/paynow", methods=["GET", "POST"])
+def paynow():
+    return render_template("paynow.html")
 
 if __name__=="__main__":
     app.run(port=8000, debug=True)
